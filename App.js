0fd59d8222906f75a9fad60972d712cba985c1c2
AlexandrGreat <109344446+AlexandrGreat@users.noreply.github.com>
2023-10-25 10:31:55 +0300

    Update App.js

--- App.js
+++ App.js
@@ -1,101 +1,136 @@
-import { StatusBar } from 'expo-status-bar';
-import React,{useState} from 'react';
-import { Alert, StyleSheet, Text, TouchableHighlight, View, Button, ScrollView, TextInput } from 'react-native';
+import React, {useEffect, useRef} from "react";
+import image1 from './images/image1.png';
+import image2 from './images/image2.png';
+import image3 from './images/image3.png';
+import image4 from './images/image4.png';
+import { SafeAreaView,ScrollView,Text,StyleSheet,View,ImageBackground,Animated,useWindowDimensions } from "react-native";
 
-class App extends React.Component
-{
-  state={
-    buttonClickCounter:0,
-    someText:'Sample text',
-  }
+const images=[image1,image2,image3,image4];
 
-  updateCounter(){
-    Count=this.state.buttonClickCounter;
-    this.setState({buttonClickCounter:Count+1});
-  }
+const App=()=>{
 
-  dropCounter(){
-    this.setState({buttonClickCounter:0})
-  }
+    const scrollX=useRef(new Animated.Value(0)).current;
+    const{width:windowWidth}=useWindowDimensions();
 
-  onButtonClick(msg){
-    alert('Current inputed text: '+msg)
-  }
+    moveValue1=new Animated.Value(-200);
+    moveValue2=new Animated.Value(-300);
+    moveValue3=new Animated.Value(-300);
+    moveValue4=new Animated.Value(-700);
+  
+    useEffect(()=>{animate()},[])
+  
+    const animate=()=>
+    {
+      Animated.parallel([
+        Animated.timing(
+          this.moveValue1,{toValue:0,duration:2000}
+        ),
+        Animated.timing(
+          this.moveValue2,{toValue:0,duration:2000,delay:2000}
+        ),
+        Animated.timing(
+          this.moveValue3,{toValue:0,duration:2000,delay:4000}
+        ),
+        Animated.timing(
+          this.moveValue4,{toValue:0,duration:2000,delay:6000}
+        )
+      ]).start();
+    }
 
-  render(){
     return(
-      <View  style={styles.container}>     
-        <Text style={styles.headText}>Welcome to the most useless clicker!</Text>
-          <View style={styles.mainContainer}>
-          <View style={styles.innerContainer}>
-            <Text>You clicked button {this.state.buttonClickCounter} times</Text>
-          </View>
-            <View style={styles.button1}><Button onPress={()=>this.updateCounter()} title='Click me!'></Button></View>
-            <View style={styles.button1}><Button onPress={()=>this.dropCounter()} title='Drop value!'></Button></View>
-          </View>
-
-
-        <ScrollView style={styles.scrollViewStyle}>
-          <Text style={{color:'red'}}>Inputed text here:</Text>
-          <TextInput style={styles.input} multiline={true} value={this.state.someText} onChangeText={newText=>this.setState({someText:newText})}></TextInput>
-          <Text style={{color:'red'}}>Inputed text is:</Text>
-          <Text style={styles.input}>{this.state.someText}</Text>
-          <Button title='Click me!' onPress={()=>this.onButtonClick(this.state.someText)}></Button>
-        </ScrollView>
-      </View>
+        <SafeAreaView style={styles.container}>
+            <View style={styles.greeting}>
+                    <Animated.Text style={[styles.h2, {position:'relative',top:this.moveValue1}]}>Welocome to</Animated.Text>
+                    <Animated.Text style={[styles.h1, {position:'relative',left:this.moveValue2}]}>LR8 App</Animated.Text>
+                    <Animated.Text style={[styles.h3, {position:'relative',right:this.moveValue3}]}>components animation</Animated.Text>
+            </View>
+            
+            <Animated.View style={{position:'relative',bottom:this.moveValue4}}>
+            <View style={styles.scrollContainer}>
+            <ScrollView style={styles.scrollViewStyle} horizontal={true} pagingEnabled showsHorizontalScrollIndicator={false} onScroll={Animated.event([{nativeEvent:{contentOffset:{x:scrollX}}}])} scrollEventThrottle={1}>
+                    {images.map((image,imageIndex)=>{return(<View style={{width:windowWidth,height:250}} key={imageIndex}>
+                        <ImageBackground source={image} style={styles.card}>
+                            <View style={styles.textContainer}>
+                                <Text style={styles.infoText}>
+                                    {"Image - "+(imageIndex+1)}
+                                </Text>
+                            </View>
+                        </ImageBackground>
+                    </View>);})}
+                </ScrollView>
+                <View style={styles.indicatorContainer}>
+                    {images.map((image,imageIndex)=>{const width=scrollX.interpolate({
+                        inputRange:[windowWidth*(imageIndex-1),windowWidth*imageIndex,windowWidth*(imageIndex+1)],outputRange:[8,16,8],extrapolate:'clamp'
+                    });return(<Animated.View key={imageIndex} style={[styles.normalDot,{width}]}></Animated.View>)})}
+                </View>
+            </View>
+            </Animated.View>
+                
+        </SafeAreaView>
     )
-  }
 }
 
-const styles = StyleSheet.create({
-  input:{
-    borderColor:'#7a42f4',
-    borderWidth:2
-  },
-  headText:{
-    color:'red',
-    fontWeight:'bold',
-    position:'relative',
-    top:'10%',
-    left:'20%',
-    width:'100%',
-  },
-  scrollViewStyle:{
-    backgroundColor: '#ffffff',
-    width:'70%',
-    height:'10%',
-    borderRadius:10,
-    position:'absolute',
-    top:'60%'
-  },
-  mainContainer: {
-    flex: 1,
-    backgroundColor: '#88d1f1',
-    alignItems: 'center',
-    justifyContent: 'center',
-    flexDirection:'row',
-    flexWrap:'wrap',
-    position:'relative',
-    top:'50%'
-  },
-  container: {
-    flex: 1,
-    backgroundColor: '#88d1f1',
-    alignItems: 'center',
-    justifyContent: 'center',
-    height:'100%'
-  },
-  innerContainer: {
-    backgroundColor: '#ffffff',
-    alignItems: 'center',
-    justifyContent: 'center',
-    width:'100%',
-    height:'10%',
-    borderRadius:10
-  },
-  button1:{
-    width:'50%'
-  }
+const styles=StyleSheet.create({
+    container:{
+        flex:1,
+        alignItems:'center',
+        justifyContent:'center',
+    },
+    scrollContainer:{
+        height:300,
+        alignContent:'center',
+        alignItems:'center',
+    },
+    card:{
+        flex:1,
+        marginVertical:4,
+        marginHorizontal:16,
+        borderRadius:5,
+        overflow:'hidden',
+        alignItems:'center',
+        justifyContent:'center',
+    },
+    textContainer:{
+        backgroundColor:'rgba(0,0,0,0.7)',
+        paddingHorizontal:24,
+        paddingVertical:8,
+        borderRadius:5,
+    },
+    infoText:{
+        color:'white',
+        fontSize:16,
+        fontWeight:'bold'
+    },
+    normalDot:{
+        height:8,
+        width:8,
+        borderRadius:4,
+        backgroundColor:'silver',
+        marginHorizontal:4,
+    },
+    indicatorContainer:{
+        flexDirection:"row",
+        alignItems:'center',
+        justifyContent:'center'
+    },
+    greeting:{
+        alignItems:'center',
+        justifyContent:'center',
+        position:'relative',
+        top:'0%'
+    },
+    h1:{
+        color:'red',
+        fontSize:30,
+        fontWeight:'bold'
+    },
+    h2:{
+        fontSize:20,
+        fontWeight:'bold'
+    },
+    h3:{
+        fontSize:15
+    },
 });
 
 export default App;
